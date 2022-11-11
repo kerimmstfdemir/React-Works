0d428dc59a32f96cbba6c60eb05ed227514cbc92
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-11 05:28:47 +0300

    modified app.js file.

--- 5 - UseState Hook, Forms, Mouse and Keyboard Events/forms-otherEvents/src/App.js
+++ 5 - UseState Hook, Forms, Mouse and Keyboard Events/forms-otherEvents/src/App.js
@@ -1,10 +1,11 @@
 import Form from "./components/1-forms/Form";
+import FormObject from "./components/1-forms/FormObject"
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       <Form />
-      <MouseEvent />
+      <FormObject/>
     </div>
   );
 }
